4b65827671b5bfbcd883b22a6f80295cdef476b0
Hayats <hayatudeenabdulrahman@gmail.com>
2023-10-09 14:05:04 +0100

    added validation to the database schema number field

--- phonebook/models/person.js
+++ phonebook/models/person.js
@@ -1,38 +1,46 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const uri = process.env.MONGODB_URI
+const uri = process.env.MONGODB_URI;
 
-mongoose.set('strictQuery', false)
+mongoose.set("strictQuery", false);
 
-mongoose.connect(uri, {
+mongoose
+  .connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-})
-.then(() => {
-    console.log('Connected to MongoDB');
-})
-.catch(error => {
-    console.log('error connecting to MongoDB:', error.message)
-})
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("error connecting to MongoDB:", error.message);
+  });
 
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minLength: 3,
-        required: true
-    },
-    number: String,
+  name: {
+    type: String,
+    minLength: 3,
+    required: true,
+  },
+  number: {
+    type: String,
+    minLength: 8,
+    validate: {
+        validator: (v) => /^[0-9][0-9][0-9]?-\d+$/.test(v)
+        ,
+        message: props => `${props.value} is not a valid phone number!`
+      },
+  },
 });
-  
 
-personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
-})
+personSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
 
 const Person = mongoose.model("Person", personSchema);
 
-module.exports = Person
+module.exports = Person;
