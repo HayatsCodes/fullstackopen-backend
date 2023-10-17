e32a5d92354954f944652bd228bbc3aafea3da03
Hayats <hayatudeenabdulrahman@gmail.com>
2023-10-17 16:43:00 +0100

    added test for creation of a new blog

--- part-4/blog-list/tests/blog-list_api.test.js
+++ part-4/blog-list/tests/blog-list_api.test.js
@@ -8,54 +8,49 @@ const api = supertest(app)
 
 const initialBlogs = [
     {
-      _id: "5a422a851b54a676234d17f7",
       title: "React patterns",
       author: "Michael Chan",
       url: "https://reactpatterns.com/",
       likes: 7,
-      __v: 0
     },
     {
-      _id: "5a422aa71b54a676234d17f8",
       title: "Go To Statement Considered Harmful",
       author: "Edsger W. Dijkstra",
       url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
       likes: 5,
-      __v: 0
     },
     {
-      _id: "5a422b3a1b54a676234d17f9",
       title: "Canonical string reduction",
       author: "Edsger W. Dijkstra",
       url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
       likes: 12,
-      __v: 0
     },
     {
-      _id: "5a422b891b54a676234d17fa",
       title: "First class tests",
       author: "Robert C. Martin",
       url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
       likes: 10,
-      __v: 0
     },
     {
-      _id: "5a422ba71b54a676234d17fb",
       title: "TDD harms architecture",
       author: "Robert C. Martin",
       url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
       likes: 0,
-      __v: 0
     },
     {
-      _id: "5a422bc61b54a676234d17fc",
       title: "Type wars",
       author: "Robert C. Martin",
       url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
       likes: 2,
-      __v: 0
     }  
   ]
+
+  const newBlog = {
+    title: "How to use post function in SuperTest",
+    author: "Tabnine",
+    url: "https://www.tabnine.com/code/javascript/functions/supertest/SuperTest/post",
+    likes: 2,
+  }
 
 
 beforeEach(async () => {
@@ -75,6 +70,25 @@ test('of all blog list', async () => {
 test('of defined id', async () => {
     const response = await api.get('/api/blogs')
     expect(response.body[0].id).toBeDefined()
+})
+
+test('creation of a new blog', async () => {
+    await api
+        .post('/api/blogs')
+        .set('Content-Type', 'application/json')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+    
+    const blogsInDB = (await api.get('/api/blogs')).body
+    console.log('....... ', blogsInDB.length, ' .......')
+    expect(blogsInDB).toHaveLength(initialBlogs.length + 1)
+
+    const lastBlog = blogsInDB.find(blog => blog.title === 'How to use post function in SuperTest')
+    expect(lastBlog.title).toBe(
+      'How to use post function in SuperTest'
+    )
+
 })
 
 afterAll(async () => {
