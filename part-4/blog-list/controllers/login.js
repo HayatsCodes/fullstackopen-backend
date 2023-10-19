ed96fc1e5df9c35935b890f5c5b096c2884f3643
Hayats <hayatudeenabdulrahman@gmail.com>
2023-10-19 15:04:55 +0100

    Implemented a token-based authentication

---
+++ part-4/blog-list/controllers/login.js
@@ -0,0 +1,31 @@
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+const loginRouter = require('express').Router()
+const User = require('../models/users')
+
+loginRouter.post('/', async (req, res) => {
+    const {username, password} = req.body
+
+    const user = await User.findOne({username})
+    const isCorrectPassword = user === null 
+        ? false 
+        : await bcrypt.compare(password, user.passwordHash)
+    
+    if(!(user && password)) {
+        return response.status(401).json({
+            error: 'invalid username or password'
+          })
+    }
+
+    const encodedUser = {
+        username: user.username,
+        id: user._id
+    }
+
+    const token = jwt.sign(encodedUser, process.env.SECRET_KEY, {expiresIn: 60*60})
+
+    res.status(200)
+    .send({token, username: user.username, name: user.name})
+})
+
+module.exports = loginRouter
