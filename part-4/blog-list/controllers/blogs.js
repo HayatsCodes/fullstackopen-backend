137eff13a32e6bcd08fb53063264e8010a59cba0
Hayats <hayatudeenabdulrahman@gmail.com>
2023-10-19 17:56:29 +0100

    updated the delete route

--- part-4/blog-list/controllers/blogs.js
+++ part-4/blog-list/controllers/blogs.js
@@ -2,15 +2,7 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blogs')
 const User = require('../models/users')
 const jwt = require('jsonwebtoken')
-// const { info } = require('../utils/logger')
-
-const getTokenFrom = request => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.startsWith('bearer ')) {
-    return authorization.replace('bearer ', '')
-  }
-  return null
-}
+const { info } = require('../utils/logger')
 
 blogsRouter.get('/', async (req, res) => {
     const blogs = await Blog.find({}).populate('user', 'username name')
@@ -41,7 +33,8 @@ blogsRouter.delete('/:id', async (req, res) => {
     }
 
     const blog = await Blog.findById(id)
-    if (decodedToken.id.toString() === blog.user.toString()) {
+    const user = await User.findById(decodedToken.id)
+    if (user._id.toString() === blog.user.toString()) {
       await Blog.findByIdAndDelete(id)
     } else {
       return res.status(401).json({error: 'Unathorized user'})
